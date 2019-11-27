fa876baaf4f9892be499f74efae54c655b995b47
isrobson <isrobson@email.unc.edu>
2019-11-27 17:40:26 -0500

    copy leons profile prep code

---
+++ qbic-offline/prepare.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# copied from other profile branches
+
+if [ ! -d "resources" ]; then
+  echo "[] please design and link the resources folder first"
+  exit 1
+fi
+
+python3 -m compileall config.py utils.py qbic.py
