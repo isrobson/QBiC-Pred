b607821059e4c868333d51768ff6e7de046538ec
isrobson <isrobson@email.unc.edu>
2019-11-27 19:26:50 -0500

    change filterval vals

--- qbic-offline/profile_all.sh
+++ qbic-offline/profile_all.sh
@@ -6,7 +6,7 @@ for i in .5 1 2 4
 do 
   for j in 8 16 32
   do
-    for p in 1.0 0.5 0.1 0.01 0.0001
+    for p in 1.0 0.1 0.01 0.0001
     do
       python3 qbic.py -i 'mutations/single_mutations_sample_$im.tsv' -g 'testing_resources/tflist_$j.txt' -c hg19 -o 'predictions_$im.tsv' -v $p
     done # for p
